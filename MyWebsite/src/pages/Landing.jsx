f5d8637c67c25ae1c85e55cabd972d86c2056130
clzen001 <claudius.zensen001@stud.fh-dortmund.de>
2024-03-05 16:16:22 +0100

    more styling

--- MyWebsite/src/pages/Landing.jsx
+++ MyWebsite/src/pages/Landing.jsx
@@ -20,19 +20,19 @@ function Landing() {
       </section>
 
       {/* Section 2: Navbar and Content */}
-      <section ref={section2Ref} className="h-screen w-full bg-gray-300 relative">
+      <section ref={section2Ref} className="h-screen w-full bg-slate-900 relative">
         <Navbar />
         <div className="flex flex-col justify-center items-center h-full">
-          <h2 className="text-black text-4xl">Section 2</h2>
+          <h2 className="text-white text-4xl">Section 2</h2>
           <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded absolute bottom-0 mb-4" onClick={() => scrollToSection(section3Ref)}>Scroll to Section 3</button>
         </div>
       </section>
 
       {/* Section 3: Navbar and Content */}
-      <section ref={section3Ref} className="h-screen w-full bg-gray-400 relative">
+      <section ref={section3Ref} className="h-screen w-full bg-slate-900 relative">
         <Navbar />
         <div className="flex flex-col justify-center items-center h-full">
-          <h2 className="text-black text-4xl">Section 3</h2>
+          <h2 className="text-white text-4xl">Section 3</h2>
         </div>
       </section>
     </div>
