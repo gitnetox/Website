5c7c7ddde41ab39a5575e3745eb684247b505ec6
clzen001 <claudius.zensen001@stud.fh-dortmund.de>
2024-03-19 20:27:19 +0100

    small fix

--- MyWebsite/src/pages/Landing.jsx
+++ MyWebsite/src/pages/Landing.jsx
@@ -23,14 +23,14 @@ function Landing() {
       <section ref={section2Ref} className="h-screen w-full bg-slate-900 relative">
         <Navbar />
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center text-white">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center text-white mr-28">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">I am a medicale computer science student</h1>
             <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant
                 cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic
                 tumeric truffaut hexagon try-hard chambray.</p>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded-xl shadow-2xl transition-transform duration-700 hover:scale-105" alt="hero" src='https://i.imgur.com/EV8oLI9.jpeg'></img>
+            <img className="object-cover object-center rounded-xl shadow-2xl transition-transform duration-700 hover:scale-105 w-96 h-auto" alt="hero" src='https://i.imgur.com/EV8oLI9.jpeg'></img>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center h-full">
