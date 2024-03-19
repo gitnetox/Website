a243e089a36cb4b1b83a7e9eb20a1ed27ae9e79b
clzen001 <claudius.zensen001@stud.fh-dortmund.de>
2024-03-19 19:05:03 +0100

    more hero section fixes

--- MyWebsite/src/pages/Landing.jsx
+++ MyWebsite/src/pages/Landing.jsx
@@ -22,15 +22,15 @@ function Landing() {
       {/* Section 2: Navbar and Content */}
       <section ref={section2Ref} className="h-screen w-full bg-slate-900 relative">
         <Navbar />
-        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center text-white">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">Before they sold out readymade gluten</h1>
-            <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center text-white">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">Before they sold out readymade gluten</h1>
+            <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant
                 cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic
                 tumeric truffaut hexagon try-hard chambray.</p>
           </div>
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img class="object-cover object-center rounded" alt="hero" src='../assets/BildHeadshot.JPG'></img>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img className="object-cover object-center rounded-xl shadow-2xl transition-transform duration-700 hover:scale-105" alt="hero" src='https://i.imgur.com/EV8oLI9.jpeg'></img>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center h-full">
