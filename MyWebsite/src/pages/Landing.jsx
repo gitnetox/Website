83811be439bb5bffc13e70bc5991156f36212636
clzen001 <claudius.zensen001@stud.fh-dortmund.de>
2024-03-19 19:32:07 +0100

    grammar fixes

--- MyWebsite/src/pages/Landing.jsx
+++ MyWebsite/src/pages/Landing.jsx
@@ -15,7 +15,7 @@ function Landing() {
       <section className="h-screen w-full bg-slate-900 relative">
         <div className="flex flex-col justify-center items-center h-full">
           <h1 className="text-white text-6xl">Hi, i'm Claudius!</h1>
-          <button className="mt-4 bg-green-600 text-white px-4 py-2 rounded absolute bottom-0 mb-4" onClick={() => scrollToSection(section2Ref)}>click me</button>
+          <button className="mt-4 bg-green-600 text-white px-4 py-2 rounded absolute bottom-0 mb-4" onClick={() => scrollToSection(section2Ref)}>Click me</button>
         </div>
       </section>
 
@@ -24,7 +24,7 @@ function Landing() {
         <Navbar />
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center text-white">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">Before they sold out readymade gluten</h1>
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">I am a medicale computer science student</h1>
             <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant
                 cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic
                 tumeric truffaut hexagon try-hard chambray.</p>
@@ -34,7 +34,7 @@ function Landing() {
           </div>
         </div>
         <div className="flex flex-col justify-center items-center h-full">
-          <button className="mt-4 bg-green-600 text-white px-4 py-2 rounded absolute bottom-0 mb-4" onClick={() => scrollToSection(section3Ref)}>Scroll to Section 3</button>
+          <button className="mt-4 bg-green-600 text-white px-4 py-2 rounded absolute bottom-0 mb-4" onClick={() => scrollToSection(section3Ref)}>Some projects</button>
         </div> 
       </section>
 
